1a833aaab8d548f372261ab873983a5e1837d494
Kyle Bashour <kylebshr@me.com>
2016-01-22 12:34:05 -0500

    Update to the superior '-'

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# dots
-A turing-complete programming language with only one symbol: "."
+# dash
+A turing-complete programming language with only one symbol: "-"
 
 ## Usage
 
-`python dots.py <source-file>`
+`python dash.py <source-file>`
 
 ## Advantages
 
 - Turing complete! Write any program you can think of!
-- Commenting your code is easy since all characters other than '.' are ignored!
+- Commenting your code is easy since all characters other than '-' are ignored!
 - Debugging is easy! There are only two possible causes of errors: too many or too few dots.
 - Only requires one working keyboard key to program with.
 
 ## Examples
 
-A sample `cat` program is provided that will copy stdin to stdout until it reads a 0 byte. This program is composed of precisely 43453 dots.
+A sample `cat` program is provided that will copy stdin to stdout until it reads a 0 byte. This program is composed of precisely 43453 dashes.
 
 We would provide a "Hello World" program but unfortunately it's ~4.2x10<sup>55</sup> yottabytes.
--- dash.py
+++ dash.py
@@ -21,7 +21,7 @@ def compile_file(source_file):
                 c = f.read(1)
                 if len(c) == 0:
                     eof = True
-                elif c == '.':
+                elif c == '-':
                     program_n += 1
     except IOError:
         print "Error: could not open file."
@@ -30,7 +30,7 @@ def compile_file(source_file):
     binary = bin(program_n)[3:]
 
     if len(binary) % 3 != 0:
-        print "Error: either you have too many or too few dots."
+        print "Error: either you have too many or too few dashes."
         sys.exit(1)
 
     program = []
@@ -84,7 +84,7 @@ def run(program):
         elif c == LEFT:
             pointer -= 1
             if pointer < 0:
-                print "Error: either you have too many or too few dots. (out of bounds)"
+                print "Error: either you have too many or too few dashes. (out of bounds)"
                 break
         elif c == INC:
             mem[pointer] += 1
@@ -100,13 +100,13 @@ def run(program):
             if mem[pointer] == 0:
                 pc = match_loop(program, pc)
                 if pc < 0:
-                    print "Error: either you have too many or too few dots. (unmatched loop)"
+                    print "Error: either you have too many or too few dashes. (unmatched loop)"
                     break
         elif c == LOOP_END:
             if mem[pointer] != 0:
                 pc = match_loop(program, pc)
                 if pc < 0:
-                    print "Error: either you have too many or too few dots. (unmatched loop)"
+                    print "Error: either you have too many or too few dashes. (unmatched loop)"
                     break
         else:
             pass
